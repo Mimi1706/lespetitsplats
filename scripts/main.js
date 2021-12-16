a83c691ebf00b7681bc6a3d58b3d002a96a32bcc
Mimi <hana.nguyen@outlook.fr>
2021-12-16 22:01:27 +0100

    Modification du css sur les card

--- scripts/main.js
+++ scripts/main.js
@@ -22,19 +22,19 @@ let recipesSection = $('#recipe_deck')
 
     recipesSection.append(`
     
-    <div class="col-lg-4 col-md-6 col-sm-12 mb-3 p-3" id="recipe_card" data-id=${recipe.id}>
+    <div class="col-lg-4 col-md-6 col-sm-12" id="recipe_card" data-id=${recipe.id}>
 
     <div id="recipe_image"><img src="" class="card-img-top" alt=""></div>
     <div class="card-body">
 
         <div class="row d-flex align-items-center">
-        <h2 class="card-title col-8">${recipe.name}</h2>
-        <p class="col-4" id="recipe_time"><i class="far fa-clock"></i> ${recipe.time} min</p>
+        <h2 class="card-title col-7">${recipe.name}</h2>
+        <p class="col-5" id="recipe_time"><i class="far fa-clock"></i> ${recipe.time} min</p>
         </div>
 
         <div class="row">
-        <ul class="col-6" id="recipe_ingredients_${recipe.id}"></ul>
-        <p class="col-6" id="recipe_description">${recipe.description}</p>
+        <ul class="col-5" id="recipe_ingredients_${recipe.id}"></ul>
+        <p class="col-7" id="recipe_description">${recipe.description}</p>
         </div>
 
     </div>
