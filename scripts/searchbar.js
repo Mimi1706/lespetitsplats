84fcd5700f757fc411a4304b3eeb3bdf578be602
Mimi <hana.nguyen@outlook.fr>
2021-12-20 14:38:43 +0100

    filtre ingrédient fonctionnel

--- scripts/searchbar.js
+++ scripts/searchbar.js
@@ -31,67 +31,63 @@ recipes.forEach(recipe => {
         $(`#recipe_ingredients_${recipe.id}`).append(`
         <li><b>${ingredient.ingredient}</b>: ${ingredient.quantity ? ingredient.quantity : ''} ${ingredient.unit ? ingredient.unit : ''}</li>`)
     })
-
 })
 
+// À chaque activation d'une touche du clavier
 let searchInput = document.querySelector('#search_input')
 searchInput.onkeyup = (e)=>{
+
+    document.getElementById('no_result').classList.add('hidden')
     
     let userData = e.target.value; // Entrée de l'utilisateur
 
-    if(userData.length>=3){
+    // Recherche déclenchée à partir de 3 caractères
+    if (userData.length>=3){
 
-    // Efface toutes les recettes
-    let recipesSection = document.getElementById('recipe_deck')
-    recipesSection.innerHTML = '';
+        // Efface toutes les recettes
+        for(let i =0; i<document.querySelectorAll('#recipe_card').length;i++){
+            document.querySelectorAll('#recipe_card')[i].classList.add('hidden')
+        }
 
+        // Pour chaque recette
         for (let recipe of recipes){
 
+            // Initialise la liste d'ingrédients
             let allIngredients = []
             recipe.ingredients.forEach(ingredient => {
                 allIngredients.push(ingredient.ingredient.toLowerCase())
             })
 
+            // Initialise la liste d'ustensils
             let allUstensils = recipe.ustensils.flat()
 
-            if( 
+            // Pour chaque recette qui valide les conditions suivantes:
+            if(
                 recipe.name.toLowerCase().includes(userData) ||
                 recipe.appliance.toLowerCase().includes(userData) ||
                 allUstensils.join(' ').includes(userData) ||
                 allIngredients.join(' ').includes(userData)
-            ){
-                
-                // Ajoute les recettes correspondant à la recherche
-                let recipesSection = $('#recipe_deck')
-                recipesSection.append(`
+            ){  
 
-                    <div class="col-lg-4 col-md-6 col-sm-12" id="recipe_card" data-id=${recipe.id}>
+                // Sélectionne les recettes suivantes
+                for(let i =0; i<document.querySelectorAll('#recipe_card').length;i++){
+                    // Vérifie que l'id de la recette corresponde à l'id de la recette qui valide les conditions
+                    if(recipe.id == document.querySelectorAll('#recipe_card')[i].dataset.id)
+                    document.querySelectorAll('#recipe_card')[i].classList.remove('hidden')
+                }
+            }
+        }
 
-                    <div id="recipe_image"><img src="" class="card-img-top" alt=""></div>
-                    <div class="card-body">
+        // Si la section de recettes est vide (càd aucun match de recettes)
+        if(document.getElementById('recipe_deck').innerText == ''){
+            document.getElementById('no_result').classList.remove('hidden')
+        }
+    }
 
-                        <div class="row d-flex align-items-center">
-                        <h2 class="card-title col-7">${recipe.name}</h2>
-                        <p class="col-5" id="recipe_time"><i class="far fa-clock"></i> ${recipe.time} min</p>
-                        </div>
-
-                        <div class="row">
-                        <ul class="col-5" id="recipe_ingredients_${recipe.id}"></ul>
-                        <p class="col-7" id="recipe_description">${recipe.description}</p>
-                        </div>
-
-                    </div>
-
-                    </div>
-                    `);
-
-                // Ajout les ingrédients de chaque recette
-                recipe.ingredients.forEach(ingredient =>{
-                    $(`#recipe_ingredients_${recipe.id}`).append(`
-                    <li><b>${ingredient.ingredient}</b>: ${ingredient.quantity ? ingredient.quantity : ''} ${ingredient.unit ? ingredient.unit : ''}</li>`)
-                })
-            } 
+    // Si l'entrée est inférieure à 2 caractères, on montre toutes les recettes
+    else if(userData.length<=2){
+        for(let i =0; i<document.querySelectorAll('#recipe_card').length;i++){
+            document.querySelectorAll('#recipe_card')[i].classList.remove('hidden')
         }
     }
 }
-
